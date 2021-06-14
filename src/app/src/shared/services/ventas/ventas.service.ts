3419985119da4b33853cbd85e4beeb75c37a5e3c
devzamse <nilovila@e-quipu.pe>
2021-06-13 20:06:27 -0500

    data venta

---
+++ src/app/src/shared/services/ventas/ventas.service.ts
@@ -0,0 +1,46 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { map } from 'rxjs/operators';
+import { LoginService } from '../auth/login.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class VentasService {
+  constructor(private http: HttpClient, private authSession: LoginService) {}
+
+  list() {
+    let url_api = `${environment.url}/api/v1/sales/list`;
+
+    return this.http
+      .post(
+        url_api,
+        {},
+        {
+          headers: this.authSession.headerSession(),
+        }
+      )
+      .pipe(map((data) => data));
+  }
+
+  add(data: any) {
+    let url_api = `${environment.url}/api/v1/sales/add`;
+
+    return this.http
+      .post(url_api, data, {
+        headers: this.authSession.headerSession(),
+      })
+      .pipe(map((data) => data));
+  }
+
+  delete(data: any) {
+    let url_api = `${environment.url}/api/v1/sales/delete`;
+
+    return this.http
+      .post(url_api, data, {
+        headers: this.authSession.headerSession(),
+      })
+      .pipe(map((data) => data));
+  }
+}
